57ae216800b35f55a6e019745bb890d394eb775e
Vitor Baptista <vitor@vitorbaptista.com>
2012-09-13 17:21:42 -0300

    Don't try to sign in if the user didn't authorized us in FB (fixes #12)
    If the user authorized us, we'll receive oauth_token in the signed_request. If
    not, we won't. We test it before trying to find/create the user, so we avoid
    raising an exception.

--- spec/helpers/facebook_authentication_helper_spec.rb
+++ spec/helpers/facebook_authentication_helper_spec.rb
@@ -1,22 +1,26 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe FacebookAuthenticationHelper do
+  def stub_facebook_oauth_with!(parsed_signed_request)
+    oauth_mock = mock(:parse_signed_request => parsed_signed_request)
+    Koala::Facebook::OAuth.stub!(:new).and_return(oauth_mock)
+  end
+
   describe "authentication" do
     before do
       helper.class_eval do
         attr_accessor :current_user
       end
-      params['signed_request'] = {}
-      signed_request = { 'oauth_token' => 'oauth_token' }
-      @facebook_user_data = { 'email' => 'some@email.com', 'id' => 123 }
-      oauth_mock = mock(:parse_signed_request => signed_request)
-      api_mock = mock(:get_object => @facebook_user_data)
+      params['signed_request'] = 'signed_request'
 
-      Koala::Facebook::OAuth.stub!(:new).and_return(oauth_mock)
+      @facebook_user_data = { 'email' => 'some@email.com', 'id' => 123 }
+      api_mock = mock(:get_object => @facebook_user_data)
       Koala::Facebook::API.stub!(:new).and_return(api_mock)
     end
 
-    it "should sign in as the user" do
+    it "should sign in as the user if the user has authorized the app" do
+      stub_facebook_oauth_with!({ 'oauth_token' => 'oauth_token' })
+
       helper.should_receive(:sign_in) { |user| user }
 
       current_user = helper.authenticate_facebook
@@ -25,6 +29,14 @@ describe FacebookAuthenticationHelper do
       current_user.email.should == @facebook_user_data['email']
       current_user.facebook_id.should == @facebook_user_data['id']
       current_user.should_not be_new_record
+    end
+
+    it "should not try to sign in if the user has not authorized the app" do
+      stub_facebook_oauth_with!({})
+
+      helper.should_not_receive(:sign_in)
+
+      helper.authenticate_facebook
     end
   end
 
